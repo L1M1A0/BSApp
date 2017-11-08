f57431cbe9c9cfafea15f3c269c403ec104bc2d2
LZBiao <foolyidai@126.com>
2017-11-08 16:15:21 +0800

    Initial Commit

---
+++ BSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSApp
+//
+//  Created by 李振彪 on 2017/11/8.
+//  Copyright © 2017年 李振彪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
